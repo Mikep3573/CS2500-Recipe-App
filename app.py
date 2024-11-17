0139f1eaa18753c27e0c8c1e503bf8f7b8e4e8f3
Mikep3573 <m.piscione3456@gmail.com>
2024-11-17 18:36:46 -0500

    Finished basic statistical queries, started work on view recipes using where clause

--- app.py
+++ app.py
@@ -5,12 +5,19 @@ Date:
 """
 
 # Dependencies
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from gen_funcs import *
 import statistics
+import random
+import string
 
 # Setup the Flask app
 app = Flask(__name__)
+
+# Setup the session secret key
+rand_num = random.randint(16, 32)
+chars = string.ascii_letters + string.digits
+app.secret_key = ''.join(random.choice(chars) for _ in range(rand_num))
 
 @app.route("/")
 def index():
@@ -23,6 +30,65 @@ def recipes():
     """
     """
     return render_template("recipes.html")
+
+@app.route("/recipes_where")
+def recipes_where():
+    """
+    """
+    cols = [col[0] for col in get_all_cols()]
+    return render_template("recipes_where.html", cols=cols)
+
+@app.route("/where_query", methods=['POST', 'GET'])
+def where_query():
+    # Get selected column and setup the session
+    selected_col = request.form.get('selected_column')
+    selected_comp = request.form.get('selected_comp')
+    selected_t_comp = request.form.get('selected_t_comp')
+    if 'cols' not in session.keys():
+        session['cols'] = [col[0] for col in get_all_cols()]
+    if 'comps' not in session.keys():
+        session['comps'] = ['=', '!=', '>', '>=', '<', '<=']
+    if 't_comps' not in session.keys():
+        session['t_comps'] = ['=', '!=']
+    print(selected_col, selected_comp, selected_t_comp)
+
+    # Check if the column is text or numeric
+    if not selected_comp and not selected_t_comp:
+        text_cols = [col[0] for col in get_text_cols()]
+        session['text'] = selected_col in text_cols
+
+    # Update the list of columns
+    if selected_col:
+        cols = [col[0] for col in get_all_cols()]
+        cols.remove(selected_col)
+        cols = [selected_col] + cols
+
+        # Save the updated list to session
+        session['cols'] = cols
+    
+    # Update the list of comparison operators (numeric)
+    if selected_comp:
+        comps = ['=', '!=', '>', '>=', '<', '<=']
+        comps.remove(selected_comp)
+        comps = [selected_comp] + comps
+
+        # Save the updated list to session
+        session['comps'] = comps
+
+    # Update the list of comparison operators (text)
+    if selected_t_comp:
+        t_comps = ['=', '!=']
+        t_comps.remove(selected_t_comp)
+        t_comps = [selected_t_comp] + t_comps
+
+        # Save the updated list to session
+        session['t_comps'] = t_comps
+
+    return render_template("recipes_where.html", 
+                           cols=session['cols'], 
+                           text=session['text'], 
+                           comps=session['comps'], 
+                           t_comps=session['t_comps'])
 
 @app.route("/recipes_edit")
 def edit_recipes():
@@ -68,7 +134,7 @@ def stat_choice():
         # For MAX, MIN, and AVG
         cur.execute(
                 f"SELECT {selected_agg}({selected_col})" \
-                f"FROM {table}"
+                f" FROM {table}"
             )
         result = round(cur.fetchone()[0], 2)  # Round to 2 decimal places
     elif selected_agg == "MEDIAN":
@@ -86,9 +152,14 @@ def stat_choice():
         result = statistics.stdev(nums)
         result = round(result, 2)  # Round to 2 decimal places
     
+    # Update the list of columns
+    cols = [col[0] for col in get_numeric_cols()]
+    cols.remove(selected_col)
+    cols = [selected_col] + cols
     
-    return render_template("stat_queries.html", cols=[tup[0] for tup in tups], result=result)
+    return render_template("stat_queries.html", cols=cols, result=result)
 
 # Run the app
 if __name__ == "__main__":
+    # Start the app
     app.run(debug=True)
--- gen_funcs.py
+++ gen_funcs.py
@@ -30,7 +30,31 @@ def get_numeric_cols() -> list:
     numeric_cols = set(numeric_cols).union(set([(row[0], "Ingredients") for row in rows_ing]))
     numeric_cols = set(numeric_cols).union(set([(row[0], "Recipes") for row in rows_rec]))
     numeric_cols = list(numeric_cols)
+    return numeric_cols
+
+def get_text_cols() -> list:
+    con = sqlite3.connect(database="recipe_app.db", isolation_level=None)
+    cur = con.cursor()
+    # Can just check for TEXT since we know all required columns have this affinity
+    rows_auth = cur.execute("SELECT name" \
+                " FROM pragma_table_info('Authors')" \
+                " WHERE type LIKE 'TEXT'")  
+    cur = con.cursor()
+    rows_ing = cur.execute("SELECT name" \
+                " FROM pragma_table_info('Ingredients')" \
+                " WHERE type LIKE 'TEXT'")  
+    cur = con.cursor()
+    rows_rec = cur.execute("SELECT name" \
+                " FROM pragma_table_info('Recipes')" \
+                " WHERE type LIKE 'TEXT'")
     
-    # Create a list of options to give to stat_queries
-    cols = [col for col in numeric_cols]
-    return cols
+    # Create a list of possible text columns to query on (exlcuding any repeat columns)
+    text_cols = [(row[0], "Authors") for row in rows_auth]
+    text_cols = set(text_cols).union(set([(row[0], "Ingredients") for row in rows_ing]))
+    text_cols = set(text_cols).union(set([(row[0], "Recipes") for row in rows_rec]))
+    text_cols = list(text_cols)
+    return text_cols
+
+
+def get_all_cols() -> list:
+    return get_numeric_cols() + get_text_cols()
